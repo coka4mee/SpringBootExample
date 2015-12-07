5237b02a176e065d9c77b3008a23a2c109538518
Jonas Lanzendörfer <jonas.lanzendoerfer@gmx.de>
2015-12-07 10:11:50 +0100

    logging update for logback / slf4j

--- spring-boot-example/src/main/java/de/sample/Application.java
+++ spring-boot-example/src/main/java/de/sample/Application.java
@@ -1,6 +1,9 @@
 package de.sample;
 
+import de.sample.controller.HelloController;
 import de.sample.dao.CustomerRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -16,7 +19,14 @@ public class Application implements CommandLineRunner {
     @Autowired
     private CustomerRepository repository;
 
+    private static final Logger logger = LoggerFactory.getLogger(Application.class);
+
     public static void main(String[] args) {
+        logger.error("Message logged at ERROR level");
+        logger.warn("Message logged at WARN level");
+        logger.info("Message logged at INFO level");
+        logger.debug("Message logged at DEBUG level");
+
         SpringApplication.run(Application.class, args);
     }
 
